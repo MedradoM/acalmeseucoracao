684c6c1fd7442660f4db5b6b86103e5ce6bfc14d
medradom <matnego.pvz@outlook.com>
2025-03-08 11:29:04 -0300

    Add Drawer component and enhance WhoWeAreSection with animations and additional person details

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -2,7 +2,7 @@
 
 import { useHookFormMask } from "use-mask-input";
 import { Input } from "../ui/input";
-import { useForm } from "react-hook-form";
+import { useForm, UseFormRegister } from "react-hook-form";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import {
@@ -13,74 +13,122 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
+import { useEffect, useState } from "react";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../ui/drawer";
 
 const ContactForm = () => {
-  const { register } = useForm();
+  const { register } = useForm<{
+    name: string;
+    email: string;
+    phone: string;
+    description: string;
+  }>();
   const registerWithMask = useHookFormMask(register);
+  const [width, setWidth] = useState<number | null>(null);
+
+  useEffect(() => {
+    setWidth(document.body.clientWidth);
+  }, []);
+
+  if ((width ?? 0) > 640) {
+    return (
+      <Dialog>
+        <DialogTrigger className="px-3 py-2 text-sm rounded-lg bg-default-green text-default-black border border-default-black hover:bg-default-green/90 ">
+          Entre em contato
+        </DialogTrigger>
+        <DialogContent className="">
+          <DialogHeader>
+            <DialogTitle>Fale Conosco</DialogTitle>
+            <DialogDescription>
+              Entre em contato para saber mais ou para deixar seu comentário
+              sobre o programa.
+            </DialogDescription>
+          </DialogHeader>
+          <FormData register={register} registerWithMask={registerWithMask} />
+        </DialogContent>
+      </Dialog>
+    );
+  }
 
   return (
-    <Dialog>
-      <DialogTrigger className="px-3 py-2 text-sm rounded-lg bg-default-green text-default-black border border-default-black hover:bg-default-green/90 ">
+    <Drawer>
+      <DrawerTrigger className="px-3 py-2 text-sm rounded-lg bg-default-green text-default-black border border-default-black hover:bg-default-green/90 ">
         Entre em contato
-      </DialogTrigger>
-      <DialogContent className="">
-        <DialogHeader>
-          <DialogTitle>Fale Conosco</DialogTitle>
-          <DialogDescription>
+      </DrawerTrigger>
+      <DrawerContent className="h-[55%]">
+        <DrawerHeader>
+          <DrawerTitle>Fale Conosco</DrawerTitle>
+          <DrawerDescription>
             Entre em contato para saber mais ou para deixar seu comentário sobre
             o programa.
-          </DialogDescription>
-        </DialogHeader>
+          </DrawerDescription>
+        </DrawerHeader>
+        <FormData register={register} registerWithMask={registerWithMask} />
+      </DrawerContent>
+    </Drawer>
+  );
+};
 
-        <form className="grid gap-2 grid-cols-2 grid-rows-5 lg:pt-10 self-center">
-          <div className="row-span-1 col-span-2">
-            <Input
-              {...register("name", { required: true })}
-              type="text"
-              placeholder="Nome"
-              className="h-10"
-            />
-          </div>
-          <div className=" row-span-1 col-span-1">
-            <Input
-              {...register("email", { required: true })}
-              type="email"
-              placeholder="Email"
-              className="h-10"
-            />
-          </div>
-          <div className=" row-span-1 col-span-1">
-            <Input
-              {...registerWithMask(
-                "phone",
-                ["(99) 9999-9999", "(99) 99999-9999"],
-                {
-                  required: true,
-                  autoUnmask: true,
-                }
-              )}
-              placeholder="Telefone"
-              className="h-10"
-            />
-          </div>
-          <div className="row-span-2 col-span-2">
-            <Textarea
-              {...register("description")}
-              className="resize-none h-full"
-              placeholder="Assunto"
-            />
-          </div>
-          <div className="row-span-1 w-full h-full col-span-2">
-            <Button
-              type="submit"
-              className="size-full border-default-black bg-default-green border text-default-black hover:bg-default-green/95"
-            >
-              Enviar
-            </Button>
-          </div>
-        </form>
-      </DialogContent>
-    </Dialog>
+const FormData: React.FC<{
+  register: UseFormRegister<{
+    name: string;
+    email: string;
+    phone: string;
+    description: string;
+  }>;
+  registerWithMask: ReturnType<typeof useHookFormMask>;
+}> = ({ register, registerWithMask }) => {
+  return (
+    <form className="grid gap-2 grid-cols-2 grid-rows-6 lg:grid-rows-5 lg:pt-10 w-full px-5 lg:px-0 self-center">
+      <div className="row-span-1 col-span-2">
+        <Input
+          {...register("name", { required: true })}
+          type="text"
+          placeholder="Nome"
+          className="h-10"
+        />
+      </div>
+      <div className=" row-span-1 lg:col-span-1 col-span-2">
+        <Input
+          {...register("email", { required: true })}
+          type="email"
+          placeholder="Email"
+          className="h-10"
+        />
+      </div>
+      <div className=" row-span-1 lg:col-span-1 col-span-2">
+        <Input
+          {...registerWithMask("phone", ["(99) 9999-9999", "(99) 99999-9999"], {
+            required: true,
+            autoUnmask: true,
+          })}
+          placeholder="Telefone"
+          className="h-10"
+        />
+      </div>
+      <div className="row-span-2 col-span-2">
+        <Textarea
+          {...register("description")}
+          className="resize-none h-full"
+          placeholder="Assunto"
+        />
+      </div>
+      <div className="row-span-1 w-full h-full col-span-2">
+        <Button
+          type="submit"
+          className="size-full border-default-black bg-default-green border text-default-black hover:bg-default-green/95"
+        >
+          Enviar
+        </Button>
+      </div>
+    </form>
   );
 };
 
--- src/components/mainsection/whoWeAreSection/index.tsx
+++ src/components/mainsection/whoWeAreSection/index.tsx
@@ -2,8 +2,40 @@ import Title from "@/components/ui/title";
 import persons from "./persons.json";
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const WhoWeAreSection = () => {
+  useEffect(() => {
+    const allCards = gsap.utils.toArray<HTMLElement>(".cards");
+    const width = document.body.clientWidth;
+
+    allCards.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          opacity: 0,
+          x: (width ?? 0) > 640 ? (index === 0 ? 100 : -100) : -100,
+        },
+        {
+          opacity: 1,
+          x: 0,
+          duration: 0.3,
+          ease: "power4.out",
+          scrollTrigger: {
+            // markers: true,
+            trigger: card,
+            start: "top 60%",
+            toggleActions: "play none none none",
+          },
+        }
+      );
+    });
+  }, []);
+
   return (
     <div id="planos" className="px-[4%] lg:px-0 flex justify-center">
       <div className="container h-full flex flex-col gap-16 px-[2%] lg:px-[8%]">
@@ -16,29 +48,34 @@ const WhoWeAreSection = () => {
           {persons.map((person) => (
             <div
               key={`terapeuta-${person.id}`}
-              className="w-full flex last:flex-row-reverse hover:scale-105 transition-all duration-300 bg-default-green/10 rounded-lg"
+              className="cards w-full flex flex-col lg:flex-row lg:last:flex-row-reverse hover:scale-105 transition-all duration-300 bg-default-green/10 rounded-lg"
             >
               <div
                 className={twMerge(
-                  "w-1/2 relative h-[500px] bg-default-green flex items-center justify-center",
-                  person.id === 0 ? "lg:rounded-l-lg" : "lg:rounded-r-lg"
+                  "lg:w-1/2 relative h-[500px] bg-default-green flex items-center justify-center",
+                  person.id === 1 ? "lg:rounded-l-lg" : "lg:rounded-r-lg"
                 )}
               >
                 <p>imagem aqui</p>
               </div>
-              <div className="flex p-4 w-1/2 flex-col gap-4">
-                <h2 className="text-4xl font-bold">{person.name}</h2>
+              <div className="flex p-8 lg:w-1/2 flex-col justify-between gap-8">
+                <div className="flex flex-col gap-4">
+                  <h2 className="text-4xl font-bold">{person.name}</h2>
 
-                <span className="w-1/6 h-0 border border-default-green rounded-full" />
+                  <span className="w-1/6 h-0 border border-default-green rounded-full" />
 
-                <p className="text-sm">{person.description}</p>
+                  <p className="text-default-golden">{person.resume}</p>
+
+                  <p className="text-sm">{person.description}</p>
+                </div>
 
                 <Link
                   href={person.link}
                   type="button"
                   className="border w-fit px-5 py-2 text-sm rounded-lg border-default-black bg-default-green hover:bg-default-green/90 text-default-black"
                 >
-                  Saiba mais sobre {person.name.split(" ")[0]}
+                  Saiba mais sobre {person.id && { 2: "o", 1: "a" }[person.id]}{" "}
+                  {person.name.split(" ")[0]}
                 </Link>
               </div>
             </div>
